7a5fbfea4f00d6c5d5338eb3c0c4de09793df3f5
Tu Huynh <tu.huynh@linecorp.com>
2021-03-23 13:53:35 +0700

    improve

--- process.js
+++ process.js
@@ -1,14 +1,23 @@
 let debt = 0
 const logs = []
 
+// Hardcode
+// Tu Huynh ID: 'Ub4d35a2b56a1253264c2bcebbe89a62a'
+
 async function processMessage(originalMessage, source) {
   const message = originalMessage.trim().toLowerCase()
+
+  // Logging
+  console.log(originalMessage)
+  console.log(source)
 
   if (message.startsWith('log ')) {
     const messageArray = message.split(' ')
     const num = parseInt(messageArray[1])
-    const reason = messageArray[2] || 'No reason'
+    const [, , ...reasonArray] = messageArray
+    const reason = reasonArray.join(' ') || 'No reason'
     debt = debt + num
+
     const dt = new Date()
     const date = `${
       (dt.getMonth()+1).toString().padStart(2, '0')}/${
@@ -17,18 +26,24 @@ async function processMessage(originalMessage, source) {
       dt.getHours().toString().padStart(2, '0')}:${
       dt.getMinutes().toString().padStart(2, '0')}:${
       dt.getSeconds().toString().padStart(2, '0')}`
+
+    const isTu = source.userId === 'Ub4d35a2b56a1253264c2bcebbe89a62a'
+    const reporter = isTu ? 'TU' : 'DUY'
+
     logs.push({
       date,
       num,
-      reason
+      reason,
+      reporter
     })
-    return 'OK Noted!'
+
+    return `Okay got it! Debt now is ${debt}`
   }
 
   if (message === 'get log') {
     let msg = 'Transactions:\n'
     logs.forEach(log => {
-      msg += `Date: ${log.date}, ${log.num}, ${log.reason}\n`
+      msg += `[${log.date}] [${log.num}] [${log.reason}] by [${log.reporter}]\n`
     })
     return msg
   }
